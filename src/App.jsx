ef2adb520b3f032df118bdadc66af7088bec1f44
sasjaWijdenes <saswij88@gmail.com>
2023-01-04 12:32:12 +0000

    adds Card components from the MUI library to display the rest of the information for each area

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import {Card, CardContent} from '@mui/material';
 import { getAreaData } from './api'
 
 import './App.css'
@@ -28,6 +29,20 @@ function App() {
       <h1>Postcoders</h1>
       <SearchBar setSearchValue={setSearchValue} />
       <h2>{`Areas for ${searchValue}: ${areas.length}`}</h2>
+        {areas.map(area => (
+          <Card key={area['place name']} style={{ margin: '5px', display: 'flex', justifyContent: 'space-around'}} >
+            <CardContent >
+              {area['place name']}
+            </CardContent>
+            <CardContent>
+              {` ${area.state}(${area['state abbreviation']}) `}
+            </CardContent>
+            <CardContent>
+              {` Longitude: ${area.longitude}  Latitude: ${area.latitude}`}
+            </CardContent>
+          </Card>
+        ))}
+      
     </div>
   )
 }
