34f6ffc213dd9d358790364f1eae1f82ae5e8d4f
sasjaWijdenes <saswij88@gmail.com>
2023-01-04 11:26:51 +0000

    fixes bug where areas-count did not update due to attempting to concat api data onto a state.

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
     try {
       const areaData = await getAreaData()
 
-      areas.concat(areaData);
+      // areas.concat(areaData);
   
-      setAreas(areas);
+      setAreas(areaData);
     } catch (error) {
       window.alert("todo: fix app")
     }
